aa65c16f0b4108e249560a41af482803bb4420cb
Sangeeta Roy <sangeeta81244@gmail.com>
2023-12-16 18:53:39 +0530

    Practice

--- BasicCodingPractice_Java/Demo2_EscapeScequence.java
+++ BasicCodingPractice_Java/Demo2_EscapeScequence.java
@@ -4,7 +4,7 @@ public class Demo2_EscapeScequence {
     public static void main(String[] args) {
         System.out.println("Sangeeta Roy \nFemale \nSoftware Test Engineer\n");
         // by using \n -> we get a new line
-        System.out.println("1 \t 2 \n3 \t 4\n5 \t 6\n");
+        System.out.println("1 \t 2 \n 3 \t 4 \n 5 \t 6 \n");
         //by using \t -> we get a tab
         System.out.println("\'Software Test Engineer\'");
         System.out.println("\"Software Test Engineer\"");
